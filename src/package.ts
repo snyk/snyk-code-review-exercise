6f8f8ddd7b6ee9708a94c09b795d0d62968e07e8
Aron Carroll <aron.carroll@snyk.io>
2021-06-22 22:24:39 +0100

    TS PR review V3

--- src/package.ts
+++ src/package.ts
@@ -1,18 +1,51 @@
 import { RequestHandler } from 'express';
+import { minSatisfying } from 'semver';
+import got from 'got';
+import { NPMPackage } from './types'
 
 /**
- * @TODO: Retrieve package data from the npm registry and return it
+ * Attempts to retrieve package data from the npm registry and return it
  */
 export const getPackage: RequestHandler = async function (req, res, next) {
   const { name, version } = req.params;
-
-  console.log(`${name}@${version} requested`);
+  const dependencyTree = {};
 
   try {
+    const npmPackage: NPMPackage = await got(
+      `https://registry.npmjs.org/${name}`,
+    ).json();
+
+    const dependencies: Record<string, string> =
+      npmPackage.versions[version].dependencies ?? {};
+
+    for (const [name, range] of Object.entries(dependencies)) {
+      const subDep = await getDependencies(name, range);
+      dependencyTree[name] = { version: range, dependencies: subDep };
+    }
+
     return res
       .status(200)
-      .json({});
+      .json({ name, version, dependencies: dependencyTree });
   } catch (error) {
     return next(error);
   }
 };
+
+async function getDependencies(name: string, range: string): Promise<any> {
+  const npmPackage: NPMPackage = await got(
+    `https://registry.npmjs.org/${name}`,
+  ).json();
+
+  const v = minSatisfying(Object.keys(npmPackage.versions), range);
+  const dependencies: Record<string, any> = {};
+
+  if (v) {
+    const newDeps = npmPackage.versions[v].dependencies;
+    for (const [name, range] of Object.entries(newDeps ?? {})) {
+      const subDep = getDependencies(name, range);
+      dependencies[name] = { version: range, dependencies: subDep };
+    }
+  }
+
+  return dependencies;
+}
