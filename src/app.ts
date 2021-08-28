9398874dff1a015d2dd27c6727528def7bae57f7
Cefn Hoile <github.com@cefn.com>
2021-08-28 22:33:54 +0100

    Satisfy linter. Modernise tests

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import * as express from 'express';
+import type { Express } from 'express';
 import { getPackage } from './package';
 
 /**
  * Bootstrap the application framework
  */
-export function createApp() {
+export function createApp(): Express {
   const app = express();
 
   app.use(express.json());
--- test/package.test.ts
+++ test/package.test.ts
@@ -1,4 +1,3 @@
-import * as assert from "assert";
 import got from 'got';
 import { Server } from 'http';
 import { createApp } from '../src/app';
@@ -7,38 +6,42 @@ describe('/package/:name/:version endpoint', () => {
   let server: Server;
   let port: number;
 
-  beforeAll((done) => {
-    server = createApp().listen(0, () => {
-      const addr = server.address();
-      if (addr && typeof addr === "object") {
-        port = addr.port;
-        done();
-      } else {
-        done(new Error("Unexpected address ${addr} for server"));
-      }
-    });
+  beforeAll(async () => {
+    ({ server, port } = await new Promise<{ server: Server; port: number }>((resolve, reject) => {
+      const server = createApp().listen(0, () => {
+        const addr = server.address();
+        if (addr !== null && typeof addr !== 'string') {
+          const { port } = addr;
+          return resolve({
+            server,
+            port,
+          });
+        }
+        reject(new Error('Unexpected address ${addr} for server'));
+      });
+    }));
   });
 
-  afterAll((done) => {
-    server.close(done);
+  afterAll(async () => {
+    server.close();
   });
 
   it('responds', async () => {
     const packageName = 'react';
     const packageVersion = '16.13.0';
 
-    const res: any = await got(
-      `http://localhost:${port}/package/${packageName}/${packageVersion}`
-    );
+    const res = await got(`http://localhost:${port}/package/${packageName}/${packageVersion}`);
     const json = JSON.parse(res.body);
 
     expect(res.statusCode).toEqual(200);
     expect(json.name).toEqual(packageName);
     expect(json.version).toEqual(packageVersion);
-    expect(json.dependencies).toEqual({
-      'loose-envify': '^1.1.0',
-      'object-assign': '^4.1.1',
-      'prop-types': '^15.6.2',
-    });
+    expect(json.dependencies).toMatchInlineSnapshot(`
+      Object {
+        "loose-envify": "^1.1.0",
+        "object-assign": "^4.1.1",
+        "prop-types": "^15.6.2",
+      }
+    `);
   });
 });
