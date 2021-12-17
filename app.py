6deabbdcd96e757c21dde5f510d4b11f570be781
Pablo Goldszeft <pablo@snyk.io>
2021-12-17 22:24:53 +0200

    feat: implemented a server that returns the dependencies of a package from NPM

---
+++ app.py
@@ -0,0 +1,21 @@
+import uvicorn
+from fastapi import FastAPI
+
+from package import get_package
+from settings import SERVICE_HOST, SERVICE_LOG_LEVEL, SERVICE_PORT
+
+app = FastAPI()
+
+
+@app.get("/package/{name}/{version}", tags=["package"])
+async def get_package_view(name: str, version: str):
+    return await get_package(name, version)
+
+
+@app.get("/package/{name}", tags=["package"])
+async def get_latest_package_view(name: str):
+    return await get_package(name)
+
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host=SERVICE_HOST, port=SERVICE_PORT, log_level=SERVICE_LOG_LEVEL)
