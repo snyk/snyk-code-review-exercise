1be9ddf5b0c8aac565a2a70c26a548c6455fa154
Pablo Goldszeft <pablo@snyk.io>
2021-12-18 19:03:26 +0200

    feat: added transitive dependencies

--- tests/test_package.py
+++ tests/test_package.py
@@ -6,10 +6,27 @@ from src.app import app
 def test_get_package():
     client = TestClient(app)
 
-    response = client.get("/package/express/2.0.0")
+    response = client.get("/package/react/16.13.0")
     assert response.status_code == 200
     assert response.json() == {
-        "dependencies": {"connect": ">= 1.1.0 < 2.0.0", "mime": ">= 0.0.1", "qs": ">= 0.0.6"},
-        "name": "express",
-        "version": "2.0.0",
+        "dependencies": {
+            "loose-envify": {
+                "dependencies": {"js-tokens": {"dependencies": {}, "version": "4.0.0"}},
+                "version": "1.4.0",
+            },
+            "object-assign": {"dependencies": {}, "version": "4.1.1"},
+            "prop-types": {
+                "dependencies": {
+                    "loose-envify": {
+                        "dependencies": {"js-tokens": {"dependencies": {}, "version": "4.0.0"}},
+                        "version": "1.4.0",
+                    },
+                    "object-assign": {"dependencies": {}, "version": "4.1.1"},
+                    "react-is": {"dependencies": {}, "version": "16.13.1"},
+                },
+                "version": "15.7.2",
+            },
+        },
+        "name": "react",
+        "version": "16.13.0",
     }
